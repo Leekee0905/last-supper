835a3dec119c82a626379faf77d1fd3b0e2cd78f
GiSeong Lee <cj8928@gmail.com>
2024-09-13 17:29:19 +0900

    [update] modal 가운데 정렬

--- src/pages/MainPage/components/Modal/Modal.jsx
+++ src/pages/MainPage/components/Modal/Modal.jsx
@@ -4,18 +4,21 @@ import ReactDOM from 'react-dom';
 
 const customStyles = {
   content: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
     top: '50%',
     left: '50%',
     right: 'auto',
     bottom: 'auto',
     marginRight: '-50%',
     transform: 'translate(-50%, -50%)',
-    minWidth: "80%",
-    minHeight : '80%',
+    minWidth: '80%',
+    minHeight: '80%'
   },
   overlay: {
     backgroundColor: 'transparent',
-    zIndex : "50"
+    zIndex: '50'
   }
 };
 
@@ -25,7 +28,7 @@ const Modal = ({ children }) => {
   const hasModalOpen = useModalStore((state) => state.hasOpen);
   const setHasModalOpen = useModalStore((state) => state.setHasOpen);
   return ReactDOM.createPortal(
-    <ReactModal isOpen={hasModalOpen} style={customStyles}>
+    <ReactModal isOpen={hasModalOpen} style={customStyles} onRequestClose={() => setHasModalOpen(false)}>
       {children}
     </ReactModal>,
     document.getElementById('modal-root')
