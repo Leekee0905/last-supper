921e3dd895a3cb863fde37099011ad951d81f261
GiSeong Lee <cj8928@gmail.com>
2024-09-13 21:18:58 +0900

    [update] 로그인 시 모달 끄기

--- src/hooks/queries/auth/useLoginQuery.js
+++ src/hooks/queries/auth/useLoginQuery.js
@@ -1,15 +1,19 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { login } from '../../../api/auth';
 import useUserStore from '../../../store/useUserStore';
+import useModalStore from '../../../store/useModalStore';
+import { login } from '../../../api/auth';
 
 export const useLoginQuery = () => {
   const setHasAuthenticated = useUserStore((state) => state.setHasAuthenticated);
+  const setHasOpenModal = useModalStore((state) => state.setHasOpen);
   const queryClient = useQueryClient();
   return useMutation({
     mutationFn: (userData) => login(userData),
     onSuccess: (response) => {
       localStorage.setItem('accessToken', response.accessToken);
       setHasAuthenticated(true);
+      alert('로그인 되었습니다.');
+      setHasOpenModal(false);
       queryClient.invalidateQueries(['userProfile']);
     },
     onError: (error) => alert(error.response.data.message)
