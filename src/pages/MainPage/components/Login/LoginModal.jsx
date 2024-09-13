3348f04911e549c902afcc5173f9f05315d0326d
GiSeong Lee <cj8928@gmail.com>
2024-09-13 17:31:16 +0900

    [add] 로그인 모달 생성

--- src/pages/MainPage/components/Login/LoginModal.jsx
+++ src/pages/MainPage/components/Login/LoginModal.jsx
@@ -1,9 +1,13 @@
+import useModalStore from '../../../../store/useModalStore';
+import AuthForm from '../AuthForm';
 import Modal from '../Modal/Modal';
 
 const LoginModal = () => {
+  const setHasOpenModal = useModalStore((state) => state.setHasOpen);
   return (
     <Modal>
-      <div>hi</div>
+      <AuthForm mode={'login'} />
+      <button onClick={() => setHasOpenModal(false)}>닫기</button>
     </Modal>
   );
 };
