932a68993f4e8ed324d960599bbf281adc288cf6
Hbeom00 <bum6968@naver.com>
2024-09-13 15:51:10 +0900

    [add] 카카오맵 화면 표시

--- src/layout/components/HamburgerMenu.jsx
+++ src/layout/components/HamburgerMenu.jsx
@@ -1,15 +1,14 @@
 import { FiX } from 'react-icons/fi';
-import ModalItem from './ModalItem';
+import ModalItem from './HamburgerItem';
 
 const Modal = ({ hasModalOpen, toggleModal, hasLoggedIn, handleLoginLogout, handleSignup, handleCalculator }) => {
   return (
     <>
-      {hasModalOpen && (
-        <div className="fixed inset-0 bg-black bg-opacity-40 z-40"></div>
-      )}
+      {hasModalOpen && <div className="fixed inset-0 bg-black bg-opacity-40 z-40"></div>}
       <div
-        className={`fixed top-0 left-0 h-full w-[320px] bg-gray-100 shadow-md transform ${hasModalOpen ? 'translate-x-0' : '-translate-x-full'
-          } transition-transform duration-300 ease-in-out z-50`}
+        className={`fixed top-0 left-0 h-full w-[320px] bg-gray-100 shadow-md transform ${
+          hasModalOpen ? 'translate-x-0' : '-translate-x-full'
+        } transition-transform duration-300 ease-in-out z-50`}
       >
         <div className="p-4 flex justify-end">
           <button onClick={toggleModal}>
@@ -36,10 +35,8 @@ const ModalContent = ({ hasLoggedIn, handleLoginLogout, handleSignup, handleCalc
         onClick={handleLoginLogout}
         isLoggedIn={hasLoggedIn}
       />
-      {!hasLoggedIn && (
-        <ModalItem icon="signup" text="회원가입" onClick={handleSignup} />
-      )}
-       {hasLoggedIn && (
+      {!hasLoggedIn && <ModalItem icon="signup" text="회원가입" onClick={handleSignup} />}
+      {hasLoggedIn && (
         <ModalItem
           icon="bookmark"
           text="즐겨찾기"
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,5 +1,25 @@
+import { useEffect } from 'react';
+
 const MainPage = () => {
-  return <div></div>;
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${import.meta.env.VITE_KAKAO_API_KEY}&autoload=false`;
+    document.head.appendChild(script);
+
+    script.onload = () => {
+      window.kakao.maps.load(() => {
+        const container = document.getElementById('map');
+        const options = {
+          center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+          level: 3
+        };
+
+        const map = new window.kakao.maps.Map(container, options);
+      });
+    };
+  }, []);
+  return <div id="map" style={{ width: 'clac(100% - 400px)', height: '100%' }}></div>;
 };
 
 export default MainPage;
