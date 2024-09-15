c3a650d3ed08476b20c300a4b3780e72a5a48464
GiSeong Lee <cj8928@gmail.com>
2024-09-15 17:44:15 +0900

    [update] 모달 닫기 버튼 생성

--- src/pages/MainPage/components/Login/LoginModal.jsx
+++ src/pages/MainPage/components/Login/LoginModal.jsx
@@ -1,3 +1,4 @@
+import { FiX } from 'react-icons/fi';
 import useModalStore from '../../../../store/useModalStore';
 import AuthForm from '../AuthForm';
 import Modal from '../Modal/Modal';
@@ -6,8 +7,12 @@ const LoginModal = () => {
   const setHasOpenModal = useModalStore((state) => state.setHasOpen);
   return (
     <>
+      <div className="flex justify-end h-[5vh] pt-[1vh] pr-[1vw]">
+        <button onClick={() => setHasOpenModal(false)}>
+          <FiX className="text-3xl hover:text-[var(--black-color)] active:opacity-50" />
+        </button>
+      </div>
       <AuthForm mode={'login'} />
-      <button onClick={() => setHasOpenModal(false)}>닫기</button>
     </>
   );
 };
--- src/pages/MainPage/components/Signup/SignupModal.jsx
+++ src/pages/MainPage/components/Signup/SignupModal.jsx
@@ -1,3 +1,4 @@
+import { FiX } from 'react-icons/fi';
 import useModalStore from '../../../../store/useModalStore';
 import AuthForm from '../AuthForm';
 
@@ -5,8 +6,12 @@ const SignupModal = () => {
   const setHasOpenModal = useModalStore((state) => state.setHasOpen);
   return (
     <>
+      <div className="flex justify-end h-[5vh] pt-[1vh] pr-[1vw]">
+        <button onClick={() => setHasOpenModal(false)}>
+          <FiX className="text-3xl hover:text-[var(--black-color)] active:opacity-50" />
+        </button>
+      </div>
       <AuthForm mode="signup" />
-      <button onClick={() => setHasOpenModal(false)}>닫기</button>
     </>
   );
 };
