8a468f0b6f600ffb5e600ba9fe3152bcac16d9eb
GiSeong Lee <cj8928@gmail.com>
2024-09-17 20:16:35 +0900

    [delete] immer 미사용

--- src/store/useRestaurantsInfo.js
+++ src/store/useRestaurantsInfo.js
@@ -1,12 +1,9 @@
 import { create } from 'zustand';
-import { immer } from 'zustand/middleware/immer';
 
-const useRestaurantsStore = create(
-  immer((set) => ({
-    info: [],
-    setInfo: (data) => set(() => ({ info: data })),
-    deleteInfo: () => set(() => ({ info: [] }))
-  }))
-);
+const useRestaurantsStore = create((set) => ({
+  info: [],
+  setInfo: (data) => set(() => ({ info: data })),
+  deleteInfo: () => set(() => ({ info: [] }))
+}));
 
 export default useRestaurantsStore;
