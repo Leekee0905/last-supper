cdc0fcf55f532824ac387476438bfe1a8c43ef70
GiSeong Lee <cj8928@gmail.com>
2024-09-17 20:16:19 +0900

    [refactor] script map api사용 리액트 버전으로 변경

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,150 +1,86 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
-import axios from 'axios';
+import { Map } from 'react-kakao-maps-sdk';
+import EventMarkerContainer from './components/MapMarker/EventMarkerContainer';
+import { campSearchWordConverter } from '../../utils/campSearchWordConverter';
+import useRestaurantsStore from '../../store/useRestaurantsInfo';
 
 const MainPage = () => {
   const [param] = useSearchParams();
   const paramId = param.get('query');
+  const [markers, setMarkers] = useState([]);
+  const [map, setMap] = useState();
+  const setRestaurantsInfo = useRestaurantsStore((state) => state.setInfo);
+
+  const places = new kakao.maps.services.Places();
+
+  const getPlacesPositionForMarkers = (data) => {
+    let temp = [];
+    for (let i = 0; i < data.length; i++) {
+      temp.push(data[i]);
+    }
+    setMarkers(temp);
+    setRestaurantsInfo(temp);
+  };
+  const searchRestaurants = (bounds) => {
+    places.categorySearch('FD6', getPlacesPositionForMarkers, {
+      bounds: bounds,
+      useMapBounds: true
+    });
+  };
+
+  const keywordSearch = () => {
+    if (!map) return;
+    places.keywordSearch(campSearchWordConverter(paramId), (data, status, _pagination) => {
+      console.log(data);
+      if (status === kakao.maps.services.Status.OK) {
+        const bounds = new kakao.maps.LatLngBounds();
+        let markers = [];
+        for (var i = 0; i < data.length; i++) {
+          markers.push({
+            position: {
+              lat: data[i].y,
+              lng: data[i].x
+            },
+            content: data[i].place_name
+          });
+          bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+        }
+        // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
+        map.setBounds(bounds);
+        map.setLevel(5);
+        searchRestaurants(map.getBounds());
+      }
+    });
+  };
 
   useEffect(() => {
-    // 스크립트 동적 생성
-    const script = document.createElement('script');
-
-    script.type = 'text/javascript';
-    script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${
-      import.meta.env.VITE_KAKAO_API_KEY
-    }&libraries=services,clusterer,drawing&autoload=false`;
-
-    document.head.appendChild(script);
-
-    // 스크립트 로드된 후 실행
-    script.onload = () => {
-      window.kakao.maps.load(() => {
-        // 마커를 담을 배열입니다
-        // const markers = [];
-
-        // 마커를 클릭하면 장소명을 표출할 인포윈도우
-        let infowindow = new window.kakao.maps.InfoWindow({ zIndex: 1 });
-
-        // 지도 표시
-        const container = document.getElementById('map');
-        const options = {
-          center: new window.kakao.maps.LatLng(37.566826, 126.9786567),
-          level: 4
-        };
-
-        // 지도 생성
-        const map = new window.kakao.maps.Map(container, options);
-
-        // 장소 검색 객체를 생성
-        let ps = new window.kakao.maps.services.Places();
-
-        // 키워드로 장소를 검색
-        ps.keywordSearch(paramId, (data, status) => {
-          if (status === window.kakao.maps.services.Status.OK) {
-            let bounds = new window.kakao.maps.LatLngBounds();
-
-            for (let i = 0; i < data.length; i++) {
-              displayCenter(data[i]);
-              bounds.extend(new window.kakao.maps.LatLng(data[i].y, data[i].x));
-            }
-
-            map.setBounds(bounds);
-            map.setLevel(3);
-
-            searchRestaurants(map.getBounds());
-          }
-        });
-
-        const searchRestaurants = (bounds) => {
-          ps.categorySearch('FD6', placesSearchCB, {
-            bounds: bounds,
-            useMapBounds: true
-          });
-        };
-
-        // 키워드 검색 완료 시 호출되는 콜백함수 입니다
-        async function placesSearchCB(data, status) {
-          if (status === window.kakao.maps.services.Status.OK) {
-            try {
-              // 1. 먼저 모든 데이터를 가져옴 (GET 요청)
-              const response = await axios.get('http://localhost:5000/restaurantReviewApi');
-              const existingData = response.data;
-
-              // 2. 기존 데이터를 하나씩 삭제 (DELETE 요청)
-              const deleteRequests = existingData.map((item) =>
-                axios.delete(`http://localhost:5000/restaurantReviewApi/${item.id}`)
-              );
-              await Promise.all(deleteRequests); // 모든 DELETE 요청이 완료될 때까지 기다림
-              console.log('기존 데이터 삭제 완료');
-
-              // 3. 새로운 데이터를 POST 요청으로 서버에 전송
-              await axios.post('http://localhost:5000/restaurantReviewApi', {
-                restaurants: data
-              });
-
-              // 4. 지도에 마커를 표시
-              for (let i = 0; i < data.length; i++) {
-                displayMarker(data[i]);
-              }
-            } catch (error) {
-              console.error('데이터 처리 중 오류 발생:', error);
-            }
-          }
-        }
-
-        // 지도에 음식점 마커를 표시하는 함수
-        function displayMarker(place) {
-          // 마커를 생성하고 지도에 표시
-          let marker = new window.kakao.maps.Marker({
-            map: map,
-            position: new window.kakao.maps.LatLng(place.y, place.x)
-          });
-
-          // 마커에 클릭이벤트를 등록
-          window.kakao.maps.event.addListener(marker, 'mouseover', function () {
-            // 마커를 클릭하면 장소명이 인포윈도우에 표출
-            infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>');
-            infowindow.open(map, marker);
-          });
-
-          window.kakao.maps.event.addListener(marker, 'mouseout', function () {
-            infowindow.close();
-          });
-        }
-
-        // 지도에 마커를 표시하는 함수입니다
-        function displayCenter(place) {
-          // 마커 이미지 생성
-          const imageSrc = 'https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678111-map-marker-256.png'; // 마커 이미지 URL
-          const imageSize = new window.kakao.maps.Size(40, 40); // 마커 이미지 크기
-          const imageOption = { offset: new window.kakao.maps.Point(20, 40) }; // 이미지 중심 좌표
-
-          const markerImage = new window.kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
-
-          // 마커를 생성하고 지도에 표시
-          let centerMarker = new window.kakao.maps.Marker({
-            map: map,
-            position: new window.kakao.maps.LatLng(place.y, place.x),
-            image: markerImage // 마커 이미지 설정
-          });
-
-          // 마커에 클릭이벤트를 등록
-          window.kakao.maps.event.addListener(centerMarker, 'mouseover', function () {
-            // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
-            infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>');
-            infowindow.open(map, centerMarker);
-          });
-
-          window.kakao.maps.event.addListener(centerMarker, 'mouseout', function () {
-            infowindow.close();
-          });
-        }
-      });
-    };
-  }, [paramId]);
-
-  return <div id="map" style={{ width: 'clac(100% - 400px)', height: '100%' }}></div>;
+    keywordSearch();
+  }, [paramId, map]);
+  return (
+    <Map
+      center={{
+        lat: 37.566826,
+        lng: 126.9786567
+      }}
+      style={{
+        width: '100%',
+        height: '100%'
+      }}
+      level={5}
+      onCreate={setMap}
+    >
+      {markers.map((marker) => {
+        return (
+          <EventMarkerContainer
+            key={marker.id}
+            position={{ lat: marker.y, lng: marker.x }}
+            content={marker.place_name}
+          />
+        );
+      })}
+    </Map>
+  );
 };
 
 export default MainPage;
