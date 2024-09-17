c61fd1b78c15700dcc54f49dbbdd3c3fb85f5efe
GiSeong Lee <cj8928@gmail.com>
2024-09-17 21:54:51 +0900

    [add] 음식점 저장하는 api 생성

--- src/api/apiInstance.js
+++ src/api/apiInstance.js
@@ -3,3 +3,7 @@ import axios from 'axios';
 export const authApi = axios.create({
   baseURL: 'https://moneyfulpublicpolicy.co.kr'
 });
+
+export const restaurantsApi = axios.create({
+  baseURL: 'http://localhost:5000'
+});
