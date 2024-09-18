c82cafb9064b29ddb1f5cd3cda53c700058cbcbe
GiSeong Lee <cj8928@gmail.com>
2024-09-18 17:07:57 +0900

    [update] 음식점 데이터 가져와서 해당 교육대에만 속하는 데이터만 넘겨주기

--- src/api/restaurants.js
+++ src/api/restaurants.js
@@ -7,10 +7,9 @@ export const saveRestaurants = async (restaurantsData) => {
 
 export const getRestaurants = async (camp) => {
   try {
-    const response = await restaurantsApi.get('http://localhost:3000/restaurants');
-
+    const response = await restaurantsApi.get('/restaurants');
     // 서버에서 전체 데이터를 가져온 후 해당 키 값 필터링
-    const data = response.data[camp];
+    const data = response.data?.find((e) => Object.keys(e).includes(camp))[camp];
 
     if (data) {
       console.log(`음식점 목록 (${camp}):`, data);
