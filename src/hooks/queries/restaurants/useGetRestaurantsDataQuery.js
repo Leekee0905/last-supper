e5049f2f8e255c210607ef1eca8437494543feeb
GiSeong Lee <cj8928@gmail.com>
2024-09-19 10:34:47 +0900

    [delete] 음식점 저장 로직 삭제

--- src/hooks/queries/restaurants/useGetRestaurantsDataQuery.js
+++
@@ -1,9 +0,0 @@
-import { useQuery } from '@tanstack/react-query';
-import { getRestaurants } from '../../../api/restaurants';
-
-export const useGetRestaurantsDataQuery = (camp) => {
-  return useQuery({
-    queryKey: ['restaurants', camp],
-    queryFn: () => getRestaurants(camp)
-  });
-};
