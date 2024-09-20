ff3da01e0af6e2f3044d8bd1b90174ed172f816b
GiSeong Lee <cj8928@gmail.com>
2024-09-20 21:23:00 +0900

    [delete] 불필요 컴포넌트 삭제

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import Search from './components/Search';
+import AutoComplete from '../../components/AutoComplete';
 
 const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-3xl font-bold mb-6">어디로 입대하세요?</h1>
-      <Search />
+      <div className="w-full max-w-lg">
+        <AutoComplete mode={'home'} />
+      </div>
     </div>
   );
 };
