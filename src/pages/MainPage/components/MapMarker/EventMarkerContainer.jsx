8aac82daa47d4150ccb695ba1c99aba99c330028
GiSeong Lee <cj8928@gmail.com>
2024-09-20 21:23:51 +0900

    [style] 커스텀 오버레이 스타일 생성

--- src/pages/MainPage/components/MapMarker/EventMarkerContainer.jsx
+++ src/pages/MainPage/components/MapMarker/EventMarkerContainer.jsx
@@ -20,17 +20,22 @@ const EventMarkerContainer = ({ position, content }) => {
       />
       {isOpen && (
         <CustomOverlayMap position={{ lat: String(Number(position.lat) + 0.0002), lng: position.lng }}>
-          <div className="bg-white flex flex-col items-center w-[300px] h-[200px] p-5 rounded-lg">
+          <div className="bg-white flex flex-col items-center min-w-[350px] h-[200px] p-5 rounded-lg">
             <div className="flex flex-col w-full items-end">
               <button onClick={() => setIsOpen(false)}>
                 <FiX className="w-[20px] h-[20px]" />
               </button>
             </div>
-            <div className="w-full h-full flex flex-col justify-center gap-3">
+            <div className="w-full h-full flex flex-col justify-center gap-3 whitespace-nowrap">
               <h1 className="font-bold text-2xl">{content.place_name}</h1>
-              <p className="text-xs opacity-60">{content.address_name}</p>
-              <p>{content.category_name}</p>
-              <p>{content.place_url}</p>
+              <p className="text-xs text-gray-500">{content.category_name}</p>
+              <p>{content.address_name}</p>
+              <p className="text-sm">
+                URL:{' '}
+                <a className={`hover:underline hover:font-bold`} href={content.place_url}>
+                  {content.place_url}
+                </a>
+              </p>
             </div>
           </div>
         </CustomOverlayMap>
