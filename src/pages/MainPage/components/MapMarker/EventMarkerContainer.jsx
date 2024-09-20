c2538e835ffcc1751fc8ebf075d86b04f533e8e3
GiSeong Lee <cj8928@gmail.com>
2024-09-20 17:00:50 +0900

    [update]마커 오버레이 생성

--- src/pages/MainPage/components/MapMarker/EventMarkerContainer.jsx
+++ src/pages/MainPage/components/MapMarker/EventMarkerContainer.jsx
@@ -1,18 +1,35 @@
 import { useState } from 'react';
-import { MapMarker, useMap } from 'react-kakao-maps-sdk';
+import { FiX } from 'react-icons/fi';
+import { CustomOverlayMap, MapMarker, useMap } from 'react-kakao-maps-sdk';
 
 const EventMarkerContainer = ({ position, content }) => {
   const map = useMap();
   const [isVisible, setIsVisible] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const handleMarkerInfo = (marker) => {
+    map.panTo(marker.getPosition());
+    setIsOpen(true);
+  };
+
   return (
-    <MapMarker
-      position={position}
-      onClick={(marker) => map.panTo(marker.getPosition())}
-      onMouseOver={() => setIsVisible(true)}
-      onMouseOut={() => setIsVisible(false)}
-    >
-      {isVisible && content}
-    </MapMarker>
+    <>
+      <MapMarker
+        position={position}
+        clickable={true}
+        onClick={(marker) => handleMarkerInfo(marker)}
+        image={{ src: '/assets/markerLine2.png', size: { width: 70, height: 70 } }}
+        // onMouseOver={() => setIsVisible(true)}
+        // onMouseOut={() => setIsVisible(false)}
+      />
+      {(isVisible || isOpen) && (
+        <CustomOverlayMap position={{ lat: String(Number(position.lat) + 0.0002), lng: position.lng }}>
+          <div className="flex flex-col bg-white">
+            <button onClick={() => setIsOpen(false)}>닫기</button>
+            <div>{content}</div>
+          </div>
+        </CustomOverlayMap>
+      )}
+    </>
   );
 };
 export default EventMarkerContainer;
