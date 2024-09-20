8a1cdc45485bf39738299fad8b40503f83e069e8
GiSeong Lee <cj8928@gmail.com>
2024-09-20 21:22:46 +0900

    [style] 사이드바 헤더 색상 변경

--- src/layout/components/SidebarHeader.jsx
+++ src/layout/components/SidebarHeader.jsx
@@ -2,11 +2,11 @@ import { FiMenu } from 'react-icons/fi';
 import AutoComplete from '../../components/AutoComplete';
 
 const SidebarHeader = ({ toggleModal }) => (
-  <div className="sticky top-0 bg-gray-100 p-4">
+  <div className="sticky top-0 bg-[--dark-khaki-color] p-4">
     <div className="flex items-center justify-between">
       <div className="flex flex-col items-center mr-5">
-        <FiMenu className="text-2xl cursor-pointer" onClick={toggleModal} />
-        <span className="text-xs text-gray-500 mt-1">메뉴</span>
+        <FiMenu className="text-2xl cursor-pointer text-white" onClick={toggleModal} />
+        <span className="text-xs text-white mt-1">메뉴</span>
       </div>
       <AutoComplete />
     </div>
