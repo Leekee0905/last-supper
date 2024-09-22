c4f6808e347920c0b027dbf0b49a98be9f29db39
GiSeong Lee <cj8928@gmail.com>
2024-09-22 23:56:04 +0900

    [update] alert => toast로 변경

--- src/hooks/queries/auth/useSignupQuery.js
+++ src/hooks/queries/auth/useSignupQuery.js
@@ -1,15 +1,17 @@
 import { useMutation } from '@tanstack/react-query';
 import { register } from '../../../api/auth';
 import useModalStore from '../../../store/useModalStore';
+import { useAlertStore } from '../../../store/useAlertStore';
 
 export const useSignupQuery = () => {
   const setModalType = useModalStore((state) => state.setModalType);
+  const addAlert = useAlertStore((state) => state.addAlert);
   return useMutation({
     mutationFn: (formData) => register(formData),
     onSuccess: () => {
-      alert('회원가입 성공!');
+      addAlert('회원가입 성공!', 'success');
       setModalType('login');
     },
-    onError: (error) => alert(`회원가입 실패\n ${error.message}`)
+    onError: (error) => addAlert(`회원가입 실패\n ${error.message}`, 'error')
   });
 };
